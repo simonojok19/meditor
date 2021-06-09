be5bbb284e5c4c306b20406b14d7b47c47848bf7
simonojok19 <simonojok19@gmail.com>
2021-06-09 20:27:22 +0300

    refactored the bottom navigation items

--- lib/main.dart
+++ lib/main.dart
@@ -112,63 +112,7 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
-      bottomNavigationBar: Container(
-        height: 80,
-        color: Colors.white,
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            BottomNavItem(
-              title: "Today",
-              svgSrc: "assets/icons/calendar.svg",
-              press: () {},
-            ),
-            BottomNavItem(
-              title: "All Exercise",
-              svgSrc: "assets/icons/gym.svg",
-              press: () {},
-              isActive: true,
-            ),
-            BottomNavItem(
-              title: "Settings",
-              svgSrc: "assets/icons/Settings.svg",
-              press: () {},
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class BottomNavItem extends StatelessWidget {
-  final String svgSrc;
-  final String title;
-  final Function press;
-  final bool isActive;
-  const BottomNavItem({
-    Key? key,
-    required this.svgSrc,
-    required this.title,
-    required this.press,
-    this.isActive = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => press(),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          SvgPicture.asset(svgSrc),
-          Text(
-            title,
-            style: TextStyle(color: isActive ? kActiveIconColor : kTextColor),
-          )
-        ],
-      ),
+      bottomNavigationBar: BottomNavigationBar(),
     );
   }
 }
---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationBar extends StatelessWidget {
+  const BottomNavigationBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          BottomNavItem(
+            title: "Today",
+            svgSrc: "assets/icons/calendar.svg",
+            press: () {},
+          ),
+          BottomNavItem(
+            title: "All Exercise",
+            svgSrc: "assets/icons/gym.svg",
+            press: () {},
+            isActive: true,
+          ),
+          BottomNavItem(
+            title: "Settings",
+            svgSrc: "assets/icons/Settings.svg",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final String svgSrc;
+  final String title;
+  final Function press;
+  final bool isActive;
+  const BottomNavItem({
+    Key? key,
+    required this.svgSrc,
+    required this.title,
+    required this.press,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => press(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          SvgPicture.asset(svgSrc, color: isActive ? kActiveIconColor : kTextColor,),
+          Text(
+            title,
+            style: TextStyle(color: isActive ? kActiveIconColor : kTextColor),
+          )
+        ],
+      ),
+    );
+  }
+}
