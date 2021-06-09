9e08ef99f0a83fddecf9e79974ca39f3fa5efcfb
simonojok19 <simonojok19@gmail.com>
2021-06-09 20:56:49 +0300

    refactored some codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:meditor/constants.dart';
-import 'package:meditor/widgets/bottom_nav_bar.dart';
-import 'package:meditor/widgets/category_card.dart';
+import 'package:meditor/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,95 +23,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: size.height * .45,
-            decoration: BoxDecoration(
-                color: Color(0xFFF5CEB8),
-                image: DecorationImage(
-                    alignment: Alignment.centerLeft,
-                    image:
-                        AssetImage("assets/images/undraw_pilates_gpdb.png"))),
-          ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Container(
-                      margin: EdgeInsets.only(top: 10),
-                      alignment: Alignment.center,
-                      height: 52,
-                      width: 52,
-                      decoration: BoxDecoration(
-                          color: Color(0xFFF2BEA1), shape: BoxShape.circle),
-                      child: SvgPicture.asset("assets/icons/menu.svg"),
-                    ),
-                  ),
-                  Text(
-                    "Good Morning \nShishir",
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline4!
-                        .copyWith(fontWeight: FontWeight.w900),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 30),
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(29.5)),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: "Search",
-                          icon: SvgPicture.asset("assets/icons/search.svg"),
-                          border: InputBorder.none),
-                    ),
-                  ),
-                  Expanded(
-                    child: GridView.count(
-                      crossAxisCount: 2,
-                      childAspectRatio: .85,
-                      crossAxisSpacing: 20,
-                      mainAxisSpacing: 20,
-                      children: [
-                        CategoryCard(
-                          title: "Diet Recommendation",
-                          svgSrc: "assets/icons/Hamburger.svg",
-                        ),
-                        CategoryCard(
-                          title: "Kegel Exercises",
-                          svgSrc: "assets/icons/Excrecises.svg",
-                        ),
-                        CategoryCard(
-                          title: "Meditation",
-                          svgSrc: "assets/icons/Meditation.svg",
-                        ),
-                        CategoryCard(
-                          title: "Yoga",
-                          svgSrc: "assets/icons/yoga.svg",
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-      bottomNavigationBar: BottomNavBar(),
-    );
-  }
-}
