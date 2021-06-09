84508bf6508d6cdd78597b77d49bb17e509ba24d
simonojok19 <simonojok19@gmail.com>
2021-06-09 19:30:39 +0300

    added clip r rect

--- lib/main.dart
+++ lib/main.dart
@@ -126,37 +126,39 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(13),
-          boxShadow: [
-            BoxShadow(
-                offset: Offset(0, 17),
-                blurRadius: 17,
-                spreadRadius: -23,
-                color: kShadowColor)
-          ]),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: () {},
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Spacer(),
-                SvgPicture.asset(svgSrc),
-                Spacer(),
-                Text(
-                  title,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6!
-                      .copyWith(fontSize: 15),
-                )
-              ],
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(13),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0, 17),
+                  blurRadius: 17,
+                  spreadRadius: -23,
+                  color: kShadowColor)
+            ]),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Spacer(),
+                  SvgPicture.asset(svgSrc),
+                  Spacer(),
+                  Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6!
+                        .copyWith(fontSize: 15),
+                  )
+                ],
+              ),
             ),
           ),
         ),
