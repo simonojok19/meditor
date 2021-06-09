e7a2788f372b949668b3bd4426acffe3bcc22c8c
simonojok19 <simonojok19@gmail.com>
2021-06-09 21:08:56 +0300

    added the images

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:meditor/constants.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: size.height * .45,
+            decoration: BoxDecoration(
+                color: kBlueLightColor,
+                image: DecorationImage(
+                    image: AssetImage("assets/images/meditation_bg.png"),
+                    fit: BoxFit.fitWidth
+                )
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
