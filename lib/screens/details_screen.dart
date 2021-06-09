dca67b2da44f16752e0533507c3c6d6761dd8958
simonojok19 <simonojok19@gmail.com>
2021-06-09 22:07:52 +0300

    added all the activity cards in wrap

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -59,38 +59,15 @@ class DetailsScreen extends StatelessWidget {
                     child: SearchBar(),
                   ),
                   Wrap(
+                    spacing: 20,
+                    runSpacing: 20,
                     children: [
-                      Container(
-                        padding: EdgeInsets.all(16),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(13),
-                          boxShadow: [
-                            BoxShadow(
-                              offset: Offset(0, 17),
-                              blurRadius: 23,
-                              spreadRadius: -13,
-                              color: kShadowColor,
-                            )
-                          ]
-                        ),
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 42,
-                              width: 43,
-                              decoration: BoxDecoration(
-                                color: kBlueColor,
-                                shape: BoxShape.circle,
-                              ),
-                              child: Icon(
-                                Icons.play_arrow,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
+                      SessionCard(sessionNumber: 1, isDone: true),
+                      SessionCard(sessionNumber: 2, isDone: true),
+                      SessionCard(sessionNumber: 3, isDone: true),
+                      SessionCard(sessionNumber: 4, isDone: true),
+                      SessionCard(sessionNumber: 5, isDone: true),
+                      SessionCard(sessionNumber: 6, isDone: true),
                     ],
                   )
                 ],
@@ -100,5 +77,59 @@ class DetailsScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class SessionCard extends StatelessWidget {
+  final int sessionNumber;
+  final bool isDone;
+  const SessionCard({
+    Key? key,
+    required this.sessionNumber,
+    this.isDone = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraint) {
+      return Container(
+        width: constraint.maxWidth / 2 - 10,
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(13),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 17),
+                blurRadius: 23,
+                spreadRadius: -13,
+                color: kShadowColor,
+              )
+            ]),
+        child: Row(
+          children: [
+            Container(
+              height: 42,
+              width: 43,
+              decoration: BoxDecoration(
+                  color: isDone ? kBlueColor : Colors.white,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: kBlueColor)),
+              child: Icon(
+                Icons.play_arrow,
+                color: isDone ? Colors.white : kBlueColor,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              "Session $sessionNumber",
+              style: Theme.of(context).textTheme.subtitle,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
