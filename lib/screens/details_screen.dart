2507d5bb99ea96226ae4acc82701ddbf0c4ff7e7
simonojok19 <simonojok19@gmail.com>
2021-06-09 22:11:57 +0300

    working with clip r rect

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -92,42 +92,52 @@ class SessionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraint) {
-      return Container(
-        width: constraint.maxWidth / 2 - 10,
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(13),
-            boxShadow: [
-              BoxShadow(
-                offset: Offset(0, 17),
-                blurRadius: 23,
-                spreadRadius: -13,
-                color: kShadowColor,
-              )
-            ]),
-        child: Row(
-          children: [
-            Container(
-              height: 42,
-              width: 43,
-              decoration: BoxDecoration(
-                  color: isDone ? kBlueColor : Colors.white,
-                  shape: BoxShape.circle,
-                  border: Border.all(color: kBlueColor)),
-              child: Icon(
-                Icons.play_arrow,
-                color: isDone ? Colors.white : kBlueColor,
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(13),
+        child: Container(
+          width: constraint.maxWidth / 2 - 10,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 17),
+                  blurRadius: 23,
+                  spreadRadius: -13,
+                  color: kShadowColor,
+                )
+              ]),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 42,
+                      width: 43,
+                      decoration: BoxDecoration(
+                          color: isDone ? kBlueColor : Colors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: kBlueColor)),
+                      child: Icon(
+                        Icons.play_arrow,
+                        color: isDone ? Colors.white : kBlueColor,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Session $sessionNumber",
+                      style: Theme.of(context).textTheme.subtitle,
+                    ),
+                  ],
+                ),
               ),
             ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              "Session $sessionNumber",
-              style: Theme.of(context).textTheme.subtitle,
-            ),
-          ],
+          ),
         ),
       );
     });
