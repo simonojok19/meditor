e820ad1bad184e7c5e745113760f3b18585c100e
simonojok19 <simonojok19@gmail.com>
2021-06-09 21:34:09 +0300

    finished the header

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meditor/constants.dart';
+import 'package:meditor/widgets/search_bar.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
@@ -13,45 +14,54 @@ class DetailsScreen extends StatelessWidget {
           Container(
             height: size.height * .45,
             decoration: BoxDecoration(
-                color: kBlueLightColor,
-                image: DecorationImage(
-                    image: AssetImage("assets/images/meditation_bg.png"),
-                    fit: BoxFit.fitWidth)),
+              color: kBlueLightColor,
+              image: DecorationImage(
+                image: AssetImage("assets/images/meditation_bg.png"),
+                fit: BoxFit.fitWidth,
+              ),
+            ),
           ),
           SafeArea(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: size.height * 0.05,
-                ),
-                Text(
-                  "Meditation",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline4!
-                      .copyWith(fontWeight: FontWeight.w900),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "3-10 MIN Course",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: size.height * 0.05,
+                  ),
+                  Text(
+                    "Meditation",
+                    style: Theme.of(context).textTheme.headline4!.copyWith(
+                          fontWeight: FontWeight.w900,
+                        ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "3-10 MIN Course",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
                     width: size.width * 0.6,
                     child: Text(
                       "Live happier and healthier by learning the fundamentals of meditation and mindfulness",
-                    ))
-              ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: size.width * .5,
+                    child: SearchBar(),
+                  ),
+                ],
+              ),
             ),
-          ))
+          ),
         ],
       ),
     );
