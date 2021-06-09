44790517d05e4579e3f074fcc6d1b3228070f602
simonojok19 <simonojok19@gmail.com>
2021-06-09 21:51:35 +0300

    created the card

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -58,6 +58,41 @@ class DetailsScreen extends StatelessWidget {
                     width: size.width * .5,
                     child: SearchBar(),
                   ),
+                  Wrap(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(13),
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(0, 17),
+                              blurRadius: 23,
+                              spreadRadius: -13,
+                              color: kShadowColor,
+                            )
+                          ]
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 42,
+                              width: 43,
+                              decoration: BoxDecoration(
+                                color: kBlueColor,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(
+                                Icons.play_arrow,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
